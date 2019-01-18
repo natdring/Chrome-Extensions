c4c7846e3882b97bacbdff3f6c416d33e77111d2
Nat <natdring97@gmail.com>
2019-01-17 21:38:39 -0800

    Added dice roll

--- random/static/stuff.js
+++ random/static/stuff.js
@@ -1,4 +1,4 @@
-var c = document.getElementById("myCanvas");
+var c = document.getElementById("coin");
 var ctx = c.getContext("2d");
 var centerX = c.width / 2;
 var centerY = c.height / 2;
@@ -11,7 +11,6 @@ function drawCircle() {
     ctx.strokeStyle = '#000000';
     ctx.stroke();
 }
-drawCircle();
 function drawT() {
     ctx.font = "50px Comic Sans MS";
     ctx.fillStyle = '#000000';
@@ -28,23 +27,52 @@ function drawH() {
 }
 
 
-var side = 1;
+var coinSide = 1;
 var coinButton = document.getElementById('coinButton');
-var count = 0;
+var coinCount = 0;
 coinButton.onclick = function flip() {
-    count++;
-    document.getElementById("coinButton").textContent="Flip #"+count;
-    side = Math.floor(Math.random() * 2) + 1;
+    coinCount++;
+    document.getElementById("coinButton").textContent="Flip #"+coinCount;
+    coinSide = Math.floor(Math.random() * 2) + 1;
     ctx.clearRect(0, 0, c.width, c.height);
 
-    if(side==1){
+    if(coinSide==1){
         drawCircle();
         drawH();
     }
-    if(side==2){
+    if(coinSide==2){
         drawCircle();
         drawT();
         
     }
+    
+}
+
+function drawNum(diceSide) {
+    ctx.font = "50px Comic Sans MS";
+    ctx.fillStyle = '#000000';
+    ctx.beginPath();
+    ctx.fillText(diceSide, centerX-15, centerY+17);
+    ctx.fill();
+}
+
+var diceSide = 1;
+var diceButton = document.getElementById('diceButton');
+var diceCount = 0;
+var diceLength = 100;
+diceButton.onclick = function roll() {
+    diceCount++;
+    document.getElementById("diceButton").textContent="Roll #"+diceCount;
+    diceSide = Math.floor(Math.random() * 6) + 1;
+    ctx.clearRect(0, 0, c.width, c.height);
+    ctx.lineWidth = 3;
+    ctx.fillStyle = '#c2c2d6';
+    ctx.fillRect(c.width/2 - diceLength/2, c.height/2 - diceLength/2, diceLength, diceLength);
+
+    ctx.fillStyle = '#000000';
+    ctx.rect(c.width/2 - diceLength/2, c.height/2 - diceLength/2, diceLength, diceLength);
+    
+    ctx.stroke();
+    drawNum(diceSide);
     
 }
